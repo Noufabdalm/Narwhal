09e80776f8d5e3025a9b13f97123fe47d6d6ea9c
Aryaman Amit Mehta (k23043160) <aryaman.mehta@kcl.ac.uk>
2024-12-06 02:37:58 +0000

    Add tutor schedule functionality with tests
    - Implemented `tutor_schedule_view` to display tutor schedules that includes, upcoming lesson detail and assigned students
    - Added `tutor_schedule.html` template for schedule visualization.
    - Created comprehensive tests in `test_tutor_schedule_view.py` including edge cases for no lessons, unauthorized access, and multiple lessons.

--- code_tutors/urls.py
+++ code_tutors/urls.py
@@ -56,6 +56,8 @@ urlpatterns = [
     path('tutor_sign_up/', views.TutorSignUpView.as_view(), name='tutor_sign_up'),
     path('my-courses/', views.student_courses_view, name='student_courses'),
     path('lesson-schedule/', views.student_lesson_schedule_view, name='student_lesson_schedule'),
+    path('tutor-schedule/', views.tutor_schedule_view, name='tutor_schedule'),
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
